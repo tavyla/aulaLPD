c3711510b67030cddf4411c122fbb13f5974967f
tavyla <tavylavitoria4@gmail.com>
2024-10-08 09:44:55 -0300

    Add files via upload

---
+++ aulaLPD9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+int main (){
+int idadeTavyla = 16;
+int idadeMarcela = 13;
+
+if (idadeTavyla > idadeMarcela){
+printf ("Tavyla e mais velha que marcela!");
+}
+
+}
