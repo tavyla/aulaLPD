e455c849be595feadaa75afa077e886b1d781d96
tavyla <tavylavitoria4@gmail.com>
2024-11-25 11:30:07 -0300

    Add files via upload

---
+++ aulaLPD16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void){
+
+int n, c, r;
+
+printf ("numero:");
+scanf ("%d", &n);
+
+printf ("\ntabuada do %d\n\n", n)
+for (c=10; c>=1; c--;){
+  r= n*c; 
+  printf("%d x %2d = %d\n" n, c, r);
+}
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+}
